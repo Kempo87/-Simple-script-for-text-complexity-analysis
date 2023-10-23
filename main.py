0159dcc9924965eb920813a42f5fb64e2cc4fdcf
Mariusz Glapiak <145092022+Kempo87@users.noreply.github.com>
2023-10-23 06:18:34 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,24 @@
-text = input("Podaj tekst artykułu:")
+PUNCTUATIONS = ',.!?%'
 
-characters = len(text)
+def remove_punctuation(text):
+    for punc in PUNCTUATIONS:
+        text = text.replace(punc, ' ')
+    return text
 
-words = len(text.split())
+def compute_average_length(text):
+    '''Returns average word length (excluding numbers). Raises ZeroDivisionError if there are no words.'''
+    words = remove_punctuation(text).split()
+    without_numbers = [w for w in words if not w.isnumeric()] # Można też użyć w.isalpha(), wtedy warunek jest mocniejszy
+    if not without_numbers:
+        raise ZeroDivisionError("No words in the text")
+    lengths = [len(w) for w in without_numbers]
+    average = sum(lengths) / len(lengths) # Tutaj może pojawić się ZeroDivisionError.
+    return average
 
-average_word_lenght = (characters - words) / words 
+def main():
+    text = input('Podaj tekst: ')
+    average = compute_average_length(text)
+    print(f'Średnia długość słowa: {average:.2f}')
 
-print ("Słowa w Twoim tekście mają średnio", average_word_lenght, "znaków")
+if __name__ == "__main__":
+    main()
